0fa26b00fa295a65f0ec7eccc1b7675d43ecc6f1
Leonel Omar Sangolquiza Amancha <loa.sangolquiza@yavirac.edu.ec>
2024-07-19 11:04:17 -0500

    Implementacion de tabla de datos
    DashboardView.vue

--- fronted-vue/src/views/DashboardView.vue
+++ fronted-vue/src/views/DashboardView.vue
@@ -5,10 +5,74 @@
       <h1>inicio dahsboard</h1>
     </div>
   </div>
+
+  <div>
+          <div class="container izquierda">
+
+            <table class="table table-hover">
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Nombre</th>
+                  <th>DNI</th>
+                  <th>TELEFONO</th>
+                  <th>CORREO</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th>3</th>
+                  <td>leonel</td>
+                  <td>48459</td>
+                  <td>0985478652</td>
+                  <td>Leo@gmail.com</td>
+                </tr>
+                <tr>
+                  <th>3</th>
+                  <td>leonel</td>
+                  <td>48459</td>
+                  <td>0985478652</td>
+                  <td>Leo@gmail.com</td>
+                </tr>
+                <tr>
+                  <th>3</th>
+                  <td>leonel</td>
+                  <td>48459</td>
+                  <td>0985478652</td>
+                  <td>Leo@gmail.com</td>
+                </tr>
+                <tr>
+                  <th>3</th>
+                  <td>leonel</td>
+                  <td>48459</td>
+                  <td>0985478652</td>
+                  <td>Leo@gmail.com</td>
+                </tr>
+
+              </tbody>
+            </table>
+
+          </div>
+
+        <ejs-calendar :value="dateValue" min="minDate" :is-multi-selection="isMultiSelection">
+        </ejs-calendar>
+
+        <div id="app">
+          <ejs-chart>
+            <e-series-collection>
+              <e-series type="Column">
+
+              </e-series>
+            </e-series-collection>
+          </ejs-chart>
+        </div>
+
+      </div>
 </template>
 
 <script>
 import ProtectedNavbar from "../components/ProtectedNavbar.vue";
+
 
 export default {
   name: "Dashboard",
