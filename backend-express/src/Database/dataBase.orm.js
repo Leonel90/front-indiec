69782c6fbed899b92497e96ee1a28572bd4e883f
wiliam <wiliamputnomorales45@gmail.com>
2024-08-02 19:18:55 -0500

    cambios

--- backend-express/src/Database/dataBase.orm.js
+++ backend-express/src/Database/dataBase.orm.js
@@ -52,11 +52,9 @@ const detalleArtistaDisquera = require("../models/detalleArtistaDisquera.model")
 const detalleArtistaGrupo = require("../models/detalleArtistaGrupo.model");
 const detalleArtistaManeger = require("../models/detalleArtistaManeger.model");
 const detalleMusicaGeneromAlbumes = require("../models/detalleMusicaGeneromAlbumes.model")
-const detalleComentarioComentarios = require("../models/detalleComentarioComentarios.model");
 const detalleGeneromAlbumes = require("../models/detalleGeneromAlbumes.model");
 const detalleUbicacion = require("../models/detalleUbicacion.model");
-const segundoComentario = require("../models/segundoComentario.model");
-const comentarioEvento = require("../models/comentarioEvento.model");
+
 
 
 
@@ -85,11 +83,9 @@ const detalle_artista_disquera = detalleArtistaDisquera(sequelize, Sequelize);
 const detalle_artista_grupo = detalleArtistaGrupo(sequelize, Sequelize);
 const detalle_artista_maneger = detalleArtistaManeger(sequelize, Sequelize);
 const detalle_musica_generom_albumes = detalleMusicaGeneromAlbumes(sequelize, Sequelize);
-const detalle_comentario_comentarios = detalleComentarioComentarios(sequelize, Sequelize);
 const detalle_generom_albumes = detalleGeneromAlbumes(sequelize, Sequelize);
 const detalle_ubicacion = detalleUbicacion(sequelize, Sequelize);
-const segundo_comentario = segundoComentario(sequelize, Sequelize);
-const comentario_evento = comentarioEvento(sequelize, Sequelize);
+
 
 
 
@@ -102,7 +98,7 @@ const calificaciones = calificacionModel(sequelize, Sequelize);
 
 
 //relaciones
-sequelize.sync({ force: true }) // alter will update the database schema to match the model
+sequelize.sync({ alter: true }) // alter will update the database schema to match the model
     .then(() => {
         console.log('Database sincronizada PAPUCHO');
     })
@@ -127,11 +123,8 @@ module.exports = {
     detalle_artista_grupo,
     detalle_artista_maneger,
     detalle_musica_generom_albumes,
-    detalle_comentario_comentarios,
     detalle_generom_albumes,
     detalle_ubicacion,
-    segundo_comentario,
-    comentario_evento,
 
 
     // alejandro  tablas unicas
